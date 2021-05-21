1d5f4dac0648875d450d429a02ec176b52a6784b
Matias Coco <matiascoco99@gmail.com>
2021-05-21 20:53:51 -0300

    Fixed some issues in Procfile and deleted some spaces in settings.py

--- wardogs/settings.py
+++ wardogs/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 import os
 import sys
-from urllib.parse import urlparse
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 # BASE_DIR = Path(__file__).resolve().parent.parent
@@ -142,6 +141,7 @@ else:
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
 
 
 
