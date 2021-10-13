2ecc6ecfc9a171d7cba0301d874c4f01fbd3e26e
Co2Agencia <62067636+Co2Agencia@users.noreply.github.com>
2021-10-13 19:45:36 -0300

    Se elimina el secret key de settings.py

--- wardogs/settings.py
+++ wardogs/settings.py
@@ -22,9 +22,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-m2cy*3x3c%rdcu0ja-6p^rav*4(ty%-5f+nju5rmv1n%b%og6$'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
